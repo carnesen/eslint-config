056ddccdf4459a62e8bf9df694114d9c0f94c406
Chris Arnesen <chris.arnesen@gmail.com>
2020-05-25 00:20:42 -0700

    commit package-lock.json

---
+++ examples/example.js
@@ -0,0 +1,4 @@
+const { readFileSync } = require('fs');
+
+// eslint-disable-next-line
+console.log(readFileSync(__filename, 'utf8'));
---
+++ examples/example.ts
@@ -0,0 +1,4 @@
+import { readFileSync } from 'fs';
+
+// eslint-disable-next-line
+console.log(readFileSync(__filename, 'utf8'));
