02d0043e06b225361a8204a5e3bfb40648ae307a
Chris Arnesen <chris.arnesen@gmail.com>
2020-06-22 23:51:42 -0700

    Turn off no-return-await
    https://eslint.org/docs/rules/no-return-await

--- examples/example.ts
+++ examples/example.ts
@@ -2,3 +2,7 @@ import { readFileSync } from 'fs';
 
 // eslint-disable-next-line
 console.log(readFileSync(__filename, 'utf8'));
+
+export async function returnsAwait(): Promise<string> {
+	return await 'foo';
+}
