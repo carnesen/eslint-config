c4be03a115f662bae93852443b3ed5bb3f1b81f5
Chris Arnesen <chris.arnesen@gmail.com>
2020-06-25 00:08:06 -0700

    Add rule "no-underscore-dangle": "off"

--- examples/example.ts
+++ examples/example.ts
@@ -6,3 +6,10 @@ console.log(readFileSync(__filename, 'utf8'));
 export async function returnsAwait(): Promise<string> {
 	return await 'foo';
 }
+
+// underscore dangle is allowed
+export const foo = {
+	_bar(): string {
+		return 'baz';
+	},
+};
