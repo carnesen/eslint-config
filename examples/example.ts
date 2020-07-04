c1dc92a7d99e68eb1843ae952981f9c4e93c4027
Chris Arnesen <chris.arnesen@gmail.com>
2020-07-04 12:30:28 -0700

    4.3.0 @typescript-eslint/explicit-module-boundary-types off
    This rule prevents reasonable code like
    
    export function serialize(object: any): string {
    	return JSON.stringify(object);
    }

--- examples/example.ts
+++ examples/example.ts
@@ -14,6 +14,13 @@ export const foo = {
 	},
 };
 
+// Two classes in a module should be allowed
 export class Foo {}
-
 export class Bar {}
+
+// Should be allowed to export a function with argument type any if it makes
+// sense for the situation at hand. See
+// @typescript-eslint/explicit-module-boundary-types
+export function serialize(object: any): string {
+	return JSON.stringify(object);
+}
