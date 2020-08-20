dd3db9e8122c4e16c23f8101095acfc675511667
Chris Arnesen <chris.arnesen@gmail.com>
2020-08-19 20:26:04 -0700

    Disable react rules relates to whitespace because Prettier controls that
    Bump version to 4.5.0
    
    Update dependencies

--- examples/tsx-example.tsx
+++ examples/tsx-example.tsx
@@ -3,3 +3,11 @@ import React = require('react');
 export const Example: React.FunctionComponent = () => {
 	return <div>foo</div>;
 };
+
+export const notFoundMiddleware = (): any => {
+	return (
+		<div style={{ width: '100%' }}>
+			<div className="card">404 Not Found</div>
+		</div>
+	);
+};
